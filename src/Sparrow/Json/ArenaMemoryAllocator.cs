69c730e50752bdcdae9fc16429e2b1ee954290b3
Pawel Pekrol <ppekrol@ravendb.net>
2020-11-13 23:05:38 +0100

    RavenDB-15786 more work

--- src/Sparrow/Json/ArenaMemoryAllocator.cs
+++ src/Sparrow/Json/ArenaMemoryAllocator.cs
@@ -468,23 +468,25 @@ namespace Sparrow.Json
 
     public unsafe class UnmanagedMemoryManager : MemoryManager<byte>
     {
-        private readonly byte* _pointer;
+        private readonly byte* _address;
         private readonly int _length;
 
         public UnmanagedMemoryManager(byte* pointer, int length)
         {
-            _pointer = pointer;
+            _address = pointer;
             _length = length;
         }
 
-        public override Span<byte> GetSpan() => new Span<byte>(_pointer, _length);
+        public override Memory<byte> Memory => CreateMemory(_length);
+
+        public override Span<byte> GetSpan() => new Span<byte>(_address, _length);
 
         public override MemoryHandle Pin(int elementIndex = 0)
         {
             if (elementIndex < 0 || elementIndex >= _length)
                 throw new ArgumentOutOfRangeException(nameof(elementIndex));
 
-            return new MemoryHandle(_pointer + elementIndex);
+            return new MemoryHandle(_address + elementIndex);
         }
 
         public override void Unpin()
